5074bf89b56768cbd18b821a0e852a7ff9a7c32d
yangerkun <erkunyang@gmail.com>
2018-04-25 14:33:28 +1000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,16 @@
-(function($) {
+$( document ).ready(function() {
 
- function init() {
-    /* Sidebar height set */
-    $sidebarStyles = $('.sidebar').attr('style') || "";
-    $sidebarStyles += ' min-height: ' + $(document).height() + 'px;';
-    $('.sidebar').attr('style', $sidebarStyles);
+	/* Sidebar height set */
+	$('.sidebar').css('min-height',$(document).height());
 
-    /* Secondary contact links */
-    var $scontacts = $('#contact-list-secondary');
-    var $contactList = $('#contact-list');
+	/* Secondary contact links */
+	var scontacts = $('#contact-list-secondary');
+	var contact_list = $('#contact-list');
+	
+	scontacts.hide();
+	
+	contact_list.mouseenter(function(){ scontacts.fadeIn(); });
+	
+	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
 
-    $scontacts.hide();
-    $contactList.mouseenter(function(){ $scontacts.fadeIn(); });
-    $contactList.mouseleave(function(){ $scontacts.fadeOut(); });
-
-    /**
-     * Tags & categories tab activation based on hash value. If hash is undefined then first tab is activated.
-     */
-    function activateTab() {
-      if(['/tags.html', '/categories.html'].indexOf(window.location.pathname) > -1) {
-        var hash = window.location.hash;
-        if(hash)
-          $('.tab-pane').length && $('a[href="' + hash + '"]').tab('show');
-        else
-          $('.tab-pane').length && $($('.cat-tag-menu li a')[0]).tab('show');
-      }
-    }
-
-    // watch hash change and activate relevant tab
-    $(window).on('hashchange', activateTab);
-
-    // initial activation
-    activateTab();
-  };
-
-  // run init on document ready
-  $(document).ready(init);
-
-})(jQuery);
+});
